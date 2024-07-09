5f69a91a279bdff0b8999cf78eef35e12530edd1
Yashasvi Shankar Mani Tripathi <114483814+Myash16@users.noreply.github.com>
2024-07-09 19:07:38 +0000

    Var study done

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,14 @@
 console.log("Yash")
+const accountId = 144553
+let accountEmail = "yashpandey23@gmail.com"
+var accountPassword = "3422@waq"
+accountCity = "Jaipur"
+
+//accountId = 2 not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "234312"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
